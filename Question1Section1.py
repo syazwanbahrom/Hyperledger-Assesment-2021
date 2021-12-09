bc370ceb5a0cb0c22a486f872dd28deff5e5bc20
syazwanbahrom <90130167+syazwanbahrom@users.noreply.github.com>
2021-12-09 16:45:23 +0800

    Add files via upload

---
+++ Question1Section1.py
@@ -0,0 +1,14 @@
+number = int(input("Enter the Number : "))
+
+factorial =1
+
+if number < 0:
+    print("Cant Computer factorial for Negative Numbers")
+elif number < 2:
+    print("{}! = {}".format(number, factorial))
+else:
+    for num in range(number, 1, -1):
+        factorial = factorial * num
+
+    print("{}! = {}".format(number, factorial))
+
